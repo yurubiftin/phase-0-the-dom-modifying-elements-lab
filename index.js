34ec2a510b3a51a18d6e66560565b4746d2c4f9c
yurubahmed <yurubiftin2002@gmail.com>
2022-12-02 16:58:45 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader= document.createElement('h1')
+console.log('newHeader')
+
+newHeader.id ='victory'
+newHeader.innerHTML = "Yurub is the champion"
